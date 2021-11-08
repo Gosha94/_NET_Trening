49d978a4f98b629b9c1a12da5027260d43abdf24
Gosha94 <gerizch@rambler.ru>
2021-11-08 22:42:11 +0300

    add new project OpenCloseGenerics

---
+++ ValueVsReferenceCollections/Helpers/OperationTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ValueVsReferenceCollections.Helpers
+{
+
+    /// <summary>
+    /// Класс времени выполнения операции кода
+    /// </summary>
+    internal class OperationTimer : IDisposable
+    {
+
+        private Stopwatch _stopwatch;
+        private string _text;
+        private int _collectionCount;
+
+        public OperationTimer(string text)
+        {
+            PrepareForOperation();
+
+            this._text = text;
+            this._collectionCount = GC.CollectionCount(0);
+
+            // Команда должна быть последней в методе для точного старта отсчета
+            this._stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            //Console.WriteLine($"{0} (GCs = {1,3}) {2}");
+            Console.WriteLine("{0} (GCs = {1,3}) {2}", (this._stopwatch.Elapsed), GC.CollectionCount(0) - this._collectionCount, this._text );
+        }
+
+        private static void PrepareForOperation()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+    }
+}
---
+++ ValueVsReferenceCollections/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ValueVsReferenceCollections.Helpers;
+
+namespace ValueVsReferenceCollections
+{
+    public static class Program
+    {
+        const int TEST_SIZE = 100_000_000;
+        static void Main()
+        {
+            ValueTypePerformanceTest(TEST_SIZE);
+            ReferenceTypePerformanceTest(TEST_SIZE);
+            Console.ReadLine();
+        }
+
+        private static void ValueTypePerformanceTest(int counter)
+        {
+            using (new OperationTimer("List<int>"))
+            {
+                var intList = new List<int>();
+                for (var i = 0; i < counter; i++)
+                {
+                    intList.Add(i);        // Без упаковки
+                    var x = intList[i];    // Без распаковки
+                }
+                intList = null; // Для удаления при GC
+            }
+
+            using (new OperationTimer("ArrayList of int"))
+            {
+                var arrList = new ArrayList();
+                for (int i = 0; i < counter; i++)
+                {
+                    arrList.Add(i);          // Упаковка
+                    var x = (int)arrList[i]; // Распаковка
+                }
+                arrList = null; // Удаление GC
+            }
+        }
+
+        private static void ReferenceTypePerformanceTest(int counter)
+        {
+            using (new OperationTimer("List<string>"))
+            {
+                var strList = new List<string>();
+                for (var i = 0; i < counter; i++)
+                {
+                    strList.Add("X");      // Копирование ссылки
+                    var x = strList[i];    // Копирование ссылки
+                }
+                strList = null; // Для удаления при GC
+            }
+
+            using (new OperationTimer("ArrayList of string"))
+            {
+                var arrList = new ArrayList();
+                for (int i = 0; i < counter; i++)
+                {
+                    arrList.Add("X");           // Копирование ссылки
+                    var x = (string)arrList[i]; // Проверка преобразования и копирование ссылки
+                }
+                arrList = null; // Удаление GC
+            }
+        }
+
+    }
+}
