3f4459e8860341b80dc7bee05f31254825d69a26
Gosha94 <gerizch@rambler.ru>
2021-11-10 16:10:41 +0300

    end of generics lessons

--- GenericsConstraints/Types/UniversalClass.cs
+++ GenericsConstraints/Types/UniversalClass.cs
@@ -3,8 +3,11 @@
     class UniversalClass<TFromClass>
     {
         public static T DropGenericTypeToDefaultValue<T>()
-            => default;
-        
+            => default(T);
+
+        public static T WrongDropGenericTypeToDefaultValue<T>() 
+            => null; // CS0403 : нельзя привести null к параметру типа T, значимые типы не могут быть null
+
         public void GenericMethod<TFromMethod>(TFromClass arg1, TFromClass arg2)
         {
 
---
+++ GenericsMeeting/ErrorsWithGenerics/WrongCasting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsMeeting.ErrorsWithGenerics
+{
+    class WrongCasting
+    {
+        private static void CastingAGenericTypeVariable1<T>(T obj)
+        {
+            Int32 x = (Int32)obj;   // Ошибка синтаксиса
+            String s = (String)obj; // Ошибка синтаксиса
+        }
+
+        // Получаем Runtime Exception, т.к не контролируется входной параметр-тип (Говнокод)
+        private static void CastingAGenericTypeVariableWithRuntimeException<T>(T obj)
+        {
+            Int32 x = (Int32)(Object)obj;
+            String s = (String)(Object)obj;
+        }
+
+    }
+}
